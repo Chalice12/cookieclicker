73a5027c46380291e4a854e4f082f155bfb71d1c
Chalice12 <84185109+Chalice12@users.noreply.github.com>
2022-05-09 11:16:06 -0400

    Update special.js

--- special.js
+++ special.js
@@ -1,3 +1,2 @@
 var autoclicker = setInterval(function(){ try { document.getElementById('bigCookie').click(); } catch (err) { clearInterval(); } }, 1);
-setInterval(function() { Game.shimmers.forEach(function(shimmer) { if(shimmer.type == "golden" && shimmer.wrath == 0) { shimmer.pop() } }) }, 500);
-(function () {  Game.LoadMod('https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js');}());
+
